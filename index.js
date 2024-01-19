1e92cf0a6646d58724d7156f62bd23416fa6a049
Snehasao <snehasao123@gmail.com>
2024-01-19 22:14:52 +0530

    changes done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("khula ja simsim")
+var cusor=document.querySelector(".cursor")
+cusor.style.backgroundColor="blue"
+
+main.addEventListener("mousemove",function(e){
+
+    cusor.style.left=e.x+"px"
+    cusor.style.top=e.y+"px"
+})
